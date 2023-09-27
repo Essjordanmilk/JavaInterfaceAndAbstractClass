1183226ed1244305cd6a0162389f47a46ab89c10
Jordan_Manrique <jordanmc285@gmail.com>
2023-09-27 08:18:49 -0500

    [Change]Formato en la visualizacion por consola de la informacion

--- src/Logica/Bulbasaur.java
+++ src/Logica/Bulbasaur.java
@@ -1,31 +1,38 @@
 package Logica;
 
 public class Bulbasaur extends Pokemon implements IPlanta{
+    String Generacion = "Primera";
+    int Ratio_captura = 45;
+    int Experiencia_base = 64;
     public Bulbasaur() {
     }
 
     @Override
     protected void atacarPlacaje() {
-        System.out.println("Hola, soy Bulbasaur y este es mi ataque placaje.");
+        System.out.println("Hola, soy Bulbasaur y este es mi ataque placaje.\n");
     }
 
     @Override
     protected void atacarAraniazo() {
-        System.out.println("Hola, soy Bulbasaur y este es mi ataque Araniazo.");
+        System.out.println("Hola, soy Bulbasaur y este es mi ataque Araniazo.\n" +
+                "danio:200PS");
     }
 
     @Override
     protected void atacarMordisco() {
-        System.out.println("Hola, soy Bulbasaur y este es mi ataque Mordisco.");
+        System.out.println("Hola, soy Bulbasaur y este es mi ataque Mordisco.\n"+
+                "danio:320PS");
     }
 
     @Override
     public void atacarDrenaje() {
-        System.out.println("Hola, soy Bulbasaur y este es mi ataque Drenaje.");
+        System.out.println("Hola, soy Bulbasaur y este es mi ataque Drenaje.\n"+
+                "danio:430PS");
     }
 
     @Override
     public void atacarParalizar() {
-        System.out.println("Hola, soy Bulbasaur y este es mi ataque Paralizar.");
+        System.out.println("Hola, soy Bulbasaur y este es mi ataque Paralizar.\n"+
+                "danio:100PS");
     }
 }
